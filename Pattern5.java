17a2bbbcb227bc5cf79cb21f37323f2b95a1137f
Deepak Muduli <deepakmuduli978@gmail.com>
2026-02-21 11:45:59 +0530

    Modify and add new pattern problem

--- Pattern5.java
+++ Pattern5.java
@@ -1,17 +1,18 @@
 
+
     import java.util.Scanner;
-    public class Pattern4 {
+public class Pattern5 {
 
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         System.out.println("Enter the number of star do you want to print:");
         int num=sc.nextInt();
         for(int i=0;i<num;i++){
-            for(int k=num-i;k>0;k--){
-                System.out.print("* ");
-            }
-            for(int j=0;j<i;j++){
+            for(int k=1;k<num-i;k++){
                 System.out.print("  ");
+            }
+            for(int j=0;j<=i;j++){
+                System.out.print("* ");
             }
             
             System.out.println(" ");
@@ -24,3 +25,5 @@
 
 
 }
+
+
