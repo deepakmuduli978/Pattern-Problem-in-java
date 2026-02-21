e72b18d485f06c1849bce7cdef0f9aed2db86ebd
Deepak Muduli <deepakmuduli978@gmail.com>
2026-02-21 11:32:00 +0530

    Pattern Problem push into git hub

---
+++ Pattern4.java
@@ -0,0 +1,25 @@
+
+    import java.util.Scanner;
+    public class Pattern4 {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the number of star do you want to print:");
+        int num=sc.nextInt();
+        for(int i=0;i<num;i++){
+            for(int j=0;j<i;j++){
+                System.out.print("  ");
+            }
+            for(int k=num-i;k>0;k--){
+                System.out.print("* ");
+            }
+            System.out.println(" ");
+        }
+        }
+
+
+
+    
+
+
+}
